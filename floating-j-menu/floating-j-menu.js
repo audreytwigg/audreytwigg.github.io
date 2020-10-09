37880ff35aac6f30207f069b62a1ac39a736838a
audreytwigg <56984758+audreytwigg@users.noreply.github.com>
2020-10-08 19:07:32 -0600

    link, layout, image hover fixes

--- floating-j-menu/floating-j-menu.js
+++ floating-j-menu/floating-j-menu.js
@@ -15,28 +15,25 @@ document.write('<div id="menuJF" class="printhide"><div id="menu-HT sidemenu">')
 document.write('<a href="index.html" class="w3-bar-item w3-button w3-padding-16 w3-collapse w3-animate-left">HOME</a>');
 
 
+document.write('<a href="index.html#work" class="w3-bar-item w3-button w3-padding-16 w3-collapse w3-animate-left">PORTFOLIO</a>');
+
+
 document.write('<a href="index.html#about" class="w3-bar-item w3-button w3-padding-16 w3-collapse w3-animate-left">ABOUT</a>');
-
-
-document.write('<a href="index.html#work" class="w3-bar-item w3-button w3-padding-16 w3-collapse w3-animate-left">WORK</a>');
-
-
-document.write('<a href="index.html#resume" class="w3-bar-item w3-button w3-padding-16 w3-collapse w3-animate-left">RESUME</a>');
 
 
 document.write('<a href="#contact" class="w3-bar-item w3-button w3-padding-16 w3-collapse w3-animate-left">CONTACT</a>');
 
 
-document.write('<a href="https://www.instagram.com/audrey_twigg/" class="w3-bar-item w3-button w3-padding-16 w3-collapse w3-animate-left"><i class="fab fa-instagram fa-lg"></i></a>');
+document.write('<a href="https://www.instagram.com/audrey_twigg/" target="_blank" rel="noopener noreferrer" class="w3-bar-item w3-button w3-padding-16 w3-collapse w3-animate-left"><i class="fab fa-instagram fa-lg"></i></a>');
 
 
-document.write('<a href="https://www.behance.net/audreytwigg" class="w3-bar-item w3-button w3-padding-16 w3-collapse w3-animate-left"><i class="fab fa-behance fa-lg"></i></a>');
+document.write('<a href="https://www.behance.net/audreytwigg" target="_blank" rel="noopener noreferrer" class="w3-bar-item w3-button w3-padding-16 w3-collapse w3-animate-left"><i class="fab fa-behance fa-lg"></i></a>');
 
 
-document.write('<a href="https://www.twitch.tv/twigglybits" class="w3-bar-item w3-button w3-padding-16 w3-collapse w3-animate-left"><i class="fab fa-twitch fa-lg"></i></a>');
+document.write('<a href="https://www.twitch.tv/twigglybits" target="_blank" rel="noopener noreferrer" class="w3-bar-item w3-button w3-padding-16 w3-collapse w3-animate-left"><i class="fab fa-twitch fa-lg"></i></a>');
 
 
-document.write('<a href="https://www.linkedin.com/in/audrey-twigg-4668b1184/" class="w3-bar-item w3-button w3-padding-16 w3-collapse w3-animate-left"><i class="fa fa-linkedin fa-lg" aria-hidden="true"></i></a>');
+document.write('<a href="https://www.linkedin.com/in/audrey-twigg-4668b1184/" target="_blank" rel="noopener noreferrer" class="w3-bar-item w3-button w3-padding-16 w3-collapse w3-animate-left"><i class="fa fa-linkedin fa-lg" aria-hidden="true"></i></a>');
 
 
 // END LINKS
